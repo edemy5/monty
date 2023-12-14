1f978a79930f4838f10692460126a248f1f4b897
edemy5 <edem.amankwah@gmail.com>
2023-12-13 17:50:35 -0800

    monty

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - mont code interpreter
-* @argc: number of args
-* @argv: location of monty file
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
